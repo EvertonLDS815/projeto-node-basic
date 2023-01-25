b206c0b317ce04576422a2b15ca6cd633fd6b3ec
EvertonLDS815 <user.name>
2023-01-25 15:44:04 -0300

    voltando 1 2 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const port = process.env.PORT || 500;
+const port = process.env.PORT || process.env.DOOR;
 
 app.get("/", (req, res) => {
   res.send("Olá");
